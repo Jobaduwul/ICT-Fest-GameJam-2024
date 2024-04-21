4d40e5e3f6e6273ad3e8ccc0fdce6e0ff7aea26f
Shanta Maria <shantamaria@iut-dhaka.edu>
2024-04-22 04:34:08 +0600

    hearts for stove count added
    - not connected with actual stove count

---
+++ Assets/HealthManager.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthManager : MonoBehaviour
+{
+    public static int stoveCount = 5;
+    public int maxStoveCount = 5;
+    public Image[] hearts;
+    public Sprite redHeart;
+    public Sprite greenHeart;
+    public Sprite yellowHeart;
+    public Sprite emptyHeart;
+
+    private double percentage;
+
+    private void Awake()
+    {
+        stoveCount = maxStoveCount;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //by default everything is empty heart
+        foreach (Image heart in hearts)
+        {
+            heart.sprite = emptyHeart;
+        }
+
+        percentage = ((float)stoveCount / maxStoveCount) * 100.0f;
+
+        //temporarily commenting this to show some other stats
+        Debug.Log("Health: " + percentage);
+
+        //according to health, the full hearts are being filled
+        for (int i = 0; i < stoveCount; i++)
+        {
+            if (percentage < 40.0f && percentage > 0f)
+            {
+                hearts[i].sprite = redHeart;
+            }
+            else if (percentage < 75.0f)
+            {
+                hearts[i].sprite = yellowHeart;
+            }
+            else
+            {
+                hearts[i].sprite = greenHeart;
+            }
+
+        }
+    }
+}
