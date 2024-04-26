0459e4c9c6920d329013924d4f48fc8425b429d4
Shanta Maria <shantamaria@iut-dhaka.edu>
2024-04-26 22:16:47 +0600

    Revert "environment changes + healthbar added to stove"

--- Assets/Scripts/StoveHealth.cs
+++ Assets/Scripts/StoveHealth.cs
@@ -5,12 +5,9 @@ public class StoveHealth : MonoBehaviour
     public int maxLives; // Maximum number of lives for the stove
     public int currentLives; // Current number of lives for the stove
 
-    public HeartManager heartManager; 
-
     void Start()
     {
         currentLives = maxLives; // Initialize current lives to max lives
-        heartManager.SetMaxHealth((int)maxLives);
     }
 
     void OnCollisionEnter(Collision collision)
@@ -19,7 +16,6 @@ public class StoveHealth : MonoBehaviour
         {
             // Decrement stove's current lives
             currentLives--;
-            heartManager.SetHealth((int)currentLives);
 
             Debug.Log("Stove hit by food. Current lives: " + currentLives);
 
@@ -28,12 +24,7 @@ public class StoveHealth : MonoBehaviour
             {
                 // Destroy the stove object
                 Debug.Log("Stove destroyed.");
-
-                if (heartManager != null)
-                {
-                    Destroy(heartManager.gameObject);
-                }
-
+    
                 Destroy(gameObject);
 
                 // Notify GameManager that a stove is destroyed
@@ -44,5 +35,10 @@ public class StoveHealth : MonoBehaviour
                 }
             }
         }
+    }
+
+    int getCurrentLives()
+    {
+        return currentLives;
     }
 }
